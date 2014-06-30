77f157e288c510ad3b4c807a708b65fb4d6ffeca
Jan van der Pas <jvanderpas@digidentity.eu>
2014-06-30 10:35:51 +0200

    Add the specs for RemoteResource::Connection.headers and .headers=

--- spec/lib/remote_resource/connection_spec.rb
+++ spec/lib/remote_resource/connection_spec.rb
@@ -23,4 +23,32 @@ describe RemoteResource::Connection do
     end
   end
 
+  describe ".headers=" do
+    it "sets the headers as thread variable" do
+      expect(Thread.current['remote_resource.headers']).to eql({})
+
+      dummy_class.headers = { "Foo" => "Bar" }
+
+      expect(Thread.current['remote_resource.headers']).to eql({ "Foo" => "Bar" })
+    end
+  end
+
+  describe ".headers" do
+    before { dummy_class.headers = nil }
+
+    context "when headers are given" do
+      it "returns the merged headers with the default headers" do
+        dummy_class.headers = { "Foo" => "Bar" }
+
+        expect(dummy_class.headers).to eql({ "Foo" => "Bar", "Accept"=>"application/json" })
+      end
+    end
+
+    context "when NO headers are given" do
+      it "returns the default headers" do
+        expect(dummy_class.headers).to eql({ "Accept"=>"application/json" })
+      end
+    end
+  end
+
 end

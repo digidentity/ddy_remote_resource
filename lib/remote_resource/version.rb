080a08fa7d06f471f1ace78b86d0f7f86e139e41
Jan van der Pas <jvanderpas@digidentity.eu>
2014-07-03 09:52:57 +0200

    Bump the version to 0.1.0

--- lib/remote_resource/version.rb
+++ lib/remote_resource/version.rb
@@ -1,3 +1,3 @@
 module RemoteResource
-  VERSION = '0.0.4'
+  VERSION = '0.1.0'
 end
--- spec/lib/remote_resource/version_spec.rb
+++ spec/lib/remote_resource/version_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe RemoteResource::VERSION do
 
-  it { should eql '0.0.4' }
+  it { should eql '0.1.0' }
 end
 
 

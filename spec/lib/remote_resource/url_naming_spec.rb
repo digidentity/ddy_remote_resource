a8d2ffdbdf706f061841cfa5b1c3f9918e43a277
Jan van der Pas <jvanderpas@digidentity.eu>
2014-08-22 10:40:59 +0200

    Refactor the RemoteResource::UrlNaming module:
    - Use the RemoteResource::UrlNamingDetermination class for determining the url naming
    - Return the .base_url determined by the RemoteResource::UrlNamingDetermination object
    - Remove the .url_safe_relative_name and .relative_name, since it’s now handled by the RemoteResource::UrlNamingDetermination class

--- spec/lib/remote_resource/url_naming_spec.rb
+++ spec/lib/remote_resource/url_naming_spec.rb
@@ -58,84 +58,14 @@ describe RemoteResource::UrlNaming do
     end
   end
 
-  describe ".base_url" do
-    context "without additional options" do
-      it "returns the url" do
-        expect(dummy_class.base_url).to eql 'https://foobar.com/url_naming_dummy'
-      end
+  describe '.base_url' do
+    it 'uses the RemoteResource::UrlNamingDetermination class to determine the base_url' do
+      expect(RemoteResource::UrlNamingDetermination).to receive(:new).with(dummy_class).and_call_original
+      dummy_class.base_url
     end
 
-    context "with additional options" do
-      context ".version" do
-        it "returns the url with the version" do
-          dummy_class.version = '/api/v2'
-
-          expect(dummy_class.base_url).to eql 'https://foobar.com/api/v2/url_naming_dummy'
-
-          dummy_class.version = nil
-        end
-      end
-
-      context ".path_prefix" do
-        it "returns the url with the path_prefix" do
-          dummy_class.path_prefix = '/registration'
-
-          expect(dummy_class.base_url).to eql 'https://foobar.com/registration/url_naming_dummy'
-
-          dummy_class.path_prefix = nil
-        end
-      end
-
-      context ".path_postfix" do
-        it "returns the url with the path_postfix" do
-          dummy_class.path_postfix = '/refresh'
-
-          expect(dummy_class.base_url).to eql 'https://foobar.com/url_naming_dummy/refresh'
-
-          dummy_class.path_postfix = nil
-        end
-      end
-    end
-  end
-
-  describe ".url_safe_relative_name" do
-    context "when .collection is set truthy" do
-      it "returns the url for a plural resource" do
-        dummy_class.collection = true
-
-        expect(dummy_class.base_url).to eql 'https://foobar.com/url_naming_dummies'
-
-        dummy_class.collection = nil
-      end
-    end
-
-    context "when .collection is set falsely" do
-      it "returns the url for a singular resource" do
-        dummy_class.collection = false
-
-        expect(dummy_class.base_url).to eql 'https://foobar.com/url_naming_dummy'
-
-        dummy_class.collection = nil
-      end
-    end
-  end
-
-  describe ".relative_name" do
-    context "when .collection_name is specified" do
-      it "returns the relative name of the class without the module" do
-        expect(dummy_class.relative_name).not_to eql 'RemoteResource::UrlNamingDummy'
-        expect(dummy_class.relative_name).to eql 'UrlNamingDummy'
-      end
-    end
-
-    context "when .collection_name is NOT specified" do
-      it "returns the .collection_name" do
-        dummy_class.collection_name = :crash_dummy
-
-        expect(dummy_class.relative_name).to eql 'crash_dummy'
-
-        dummy_class.collection_name = nil
-      end
+    it 'returns the determined base_url' do
+      expect(dummy_class.base_url).to eql 'https://foobar.com/url_naming_dummy'
     end
   end
 

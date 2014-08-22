a121e356162b2ed894e639ca0ca64c052cac8e74
Jan van der Pas <jvanderpas@digidentity.eu>
2014-08-22 10:43:31 +0200

    Be consistent in the quotation style, and use single quotation marks

--- spec/lib/remote_resource/url_naming_spec.rb
+++ spec/lib/remote_resource/url_naming_spec.rb
@@ -18,39 +18,37 @@ describe RemoteResource::UrlNaming do
   let(:dummy_class) { RemoteResource::UrlNamingDummy }
   let(:dummy)       { dummy_class.new }
 
-  describe ".collection" do
+  describe '.collection' do
     let!(:original_collection) { dummy_class.collection }
 
-    context "when collection is set" do
-      it "returns the given collection" do
+    context 'when collection is set' do
+      it 'returns the given collection' do
         dummy_class.collection = true
 
-        expect(dummy_class.collection).to be_truthy
-        expect(dummy_class.collection).to eql(true)
+        expect(dummy_class.collection).to eql true
 
         dummy_class.collection = original_collection
       end
     end
 
-    context "when NO collection is set" do
-      it "returns the default collection" do
-        expect(dummy_class.collection).to be_falsey
-        expect(dummy_class.collection).to eql(false)
+    context 'when NO collection is set' do
+      it 'returns the default collection' do
+        expect(dummy_class.collection).to eql false
       end
     end
   end
 
-  pending "RemoteResource::CONFIG must be implemented first" ".app_host" do
-    context "when the env is given as an argument" do
-      it "uses the host specified in the application CONFIG for the given env" do
+  pending 'RemoteResource::CONFIG must be implemented first' '.app_host' do
+    context 'when the env is given as an argument' do
+      it 'uses the host specified in the application CONFIG for the given env' do
         stub_const("CONFIG", { development: { apps: { dummy: 'https://foobar.development.com' } } })
 
         expect(dummy_class.app_host 'dummy', 'development').to eql 'https://foobar.development.com'
       end
     end
 
-    context "when the env is NOT given as an argument" do
-      it "uses the host specified in the application CONFIG" do
+    context 'when the env is NOT given as an argument' do
+      it 'uses the host specified in the application CONFIG' do
         stub_const("CONFIG", { test: { apps: { dummy: 'https://foobar.test.com' } } })
 
         expect(dummy_class.app_host 'dummy').to eql 'https://foobar.test.com'
@@ -69,9 +67,9 @@ describe RemoteResource::UrlNaming do
     end
   end
 
-  describe ".use_relative_model_naming?" do
-    it "returns true" do
-      expect(dummy_class.use_relative_model_naming?).to be_truthy
+  describe '.use_relative_model_naming?' do
+    it 'returns true' do
+      expect(dummy_class.use_relative_model_naming?).to eql true
     end
   end
 

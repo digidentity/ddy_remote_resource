20601ccfe10d9e3aa1c3d35da972fc56dae9ef95
Jan van der Pas <jvanderpas@digidentity.eu>
2014-08-22 14:46:08 +0200

    Be consistent in the quotation style, and use single quotation marks

--- spec/lib/remote_resource/connection_spec.rb
+++ spec/lib/remote_resource/connection_spec.rb
@@ -17,17 +17,17 @@ describe RemoteResource::Connection do
   let(:dummy_class) { RemoteResource::ConnectionDummy }
   let(:dummy)       { dummy_class.new }
 
-  describe ".connection" do
-    it "uses Typhoeus::Request" do
+  describe '.connection' do
+    it 'uses Typhoeus::Request' do
       expect(dummy_class.connection).to eql Typhoeus::Request
     end
   end
 
-  describe ".content_type" do
+  describe '.content_type' do
     let!(:original_content_type) { dummy_class.content_type }
 
-    context "when content_type is set" do
-      it "returns the given content_type" do
+    context 'when content_type is set' do
+      it 'returns the given content_type' do
         dummy_class.content_type = '.html'
 
         expect(dummy_class.content_type).to eql '.html'
@@ -36,16 +36,16 @@ describe RemoteResource::Connection do
       end
     end
 
-    context "when NO content_type is set" do
-      it "returns the default content_type" do
+    context 'when NO content_type is set' do
+      it 'returns the default content_type' do
         expect(dummy_class.content_type).to eql '.json'
       end
     end
   end
 
-  describe ".headers" do
-    context "when .extra_headers are set" do
-      it "returns the default headers merged with the set .extra_headers" do
+  describe '.headers' do
+    context 'when .extra_headers are set' do
+      it 'returns the default headers merged with the set .extra_headers' do
         dummy_class.extra_headers = { "Foo" => "Bar" }
 
         expect(dummy_class.headers).to eql({ "Accept" => "application/json", "Foo" => "Bar" })
@@ -54,8 +54,8 @@ describe RemoteResource::Connection do
       end
     end
 
-    context "when NO .extra_headers are set" do
-      it "returns the default headers" do
+    context 'when NO .extra_headers are set' do
+      it 'returns the default headers' do
         expect(dummy_class.headers).to eql({ "Accept" => "application/json" })
       end
     end

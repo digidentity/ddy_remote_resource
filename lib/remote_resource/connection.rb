549625df61ac89056b8a0ae9fa813e3eae1af096
Jan van der Pas <jvanderpas@digidentity.eu>
2014-08-22 14:44:56 +0200

    Remove the RemoteResource::Connection.determined_request_url, .determined_headers, #determined_request_url and #determined_headers methods since this now handled by the RemoteResource::Request class

--- lib/remote_resource/connection.rb
+++ lib/remote_resource/connection.rb
@@ -19,38 +19,6 @@ module RemoteResource
         self.default_headers.merge self.extra_headers || {}
       end
 
-      private
-
-      def determined_request_url(connection_options = {}, id = nil)
-        base_url     = connection_options[:base_url].presence     || self.connection_options.base_url
-        content_type = connection_options[:content_type].presence || self.connection_options.content_type
-
-        if id.present?
-          "#{base_url}/#{id}#{content_type}"
-        else
-          "#{base_url}#{content_type}"
-        end
-      end
-
-      def determined_headers(connection_options = {})
-        connection_options[:default_headers] || self.connection_options.headers.merge(connection_options[:headers] || {})
-      end
-
     end
-
-    private
-
-    def determined_request_url(connection_options = {})
-      if connection_options[:collection] && self.id.present?
-        self.class.send :determined_request_url, connection_options, self.id
-      else
-        self.class.send :determined_request_url, connection_options
-      end
-    end
-
-    def determined_headers(connection_options = {})
-      self.class.send :determined_headers, connection_options
-    end
-
   end
 end

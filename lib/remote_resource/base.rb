70f2e38ac3a2250c13f92d179b20550f928f5dc0
Jan van der Pas <jvanderpas@digidentity.eu>
2015-07-30 11:28:20 +0200

    Revert "Add the RemoteResource::Base#empty? method, so that the object gives a proper response to the #present? and #blank? methods"
    This reverts commit 50d3163ab18e8c3b8e4694997d3af73edb952b3f.

--- lib/remote_resource/base.rb
+++ lib/remote_resource/base.rb
@@ -68,10 +68,6 @@ module RemoteResource
       @connection_options ||= RemoteResource::ConnectionOptions.new(self.class)
     end
 
-    def empty?
-      _response.try(:sanitized_response_body).blank?
-    end
-
     def persisted?
       id.present?
     end

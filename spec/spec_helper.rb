a875d9ae4134320dfa87f5908b9005859cccd18a
Jan van der Pas <jvanderpas@digidentity.eu>
2016-09-12 14:59:19 +0200

    Configure RSpec to allow focus on examples and run the examples at random order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,24 @@ require 'remote_resource'
 
 require 'webmock/rspec'
 require 'pry'
+
+RSpec.configure do |config|
+  # These two settings work together to allow you to limit a spec run
+  # to individual examples or groups you care about by tagging them with
+  # `:focus` metadata. When nothing is tagged with `:focus`, all examples
+  # get run.
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = :random
+
+  # Seed global randomization in this process using the `--seed` CLI option.
+  # Setting this allows you to use `--seed` to deterministically reproduce
+  # test failures related to randomization by passing the same `--seed` value
+  # as the one that triggered the failure.
+  Kernel.srand config.seed
+end
